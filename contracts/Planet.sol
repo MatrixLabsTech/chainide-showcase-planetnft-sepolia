40e730e0a8c0d10e44468ef427e561f8d200b04a
wufengtao1 <48203810+wufengtao1@users.noreply.github.com>
2023-04-26 13:35:35 +0800

    Update Planet.sol

--- contracts/Planet.sol
+++ contracts/Planet.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-// @title Theirsverse NFT
+// @title Planet NFT
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
