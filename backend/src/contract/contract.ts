4d77adff5c83f5c3fa0cc036bf131a6637e8ddb3
r1chardyang <651293826@qq.com>
2024-03-15 14:11:58 +0800

    remove

--- backend/src/contract/contract.ts
+++
@@ -1,6 +0,0 @@
-import abi from '@/config/abi.json';
-import { CONTRACT_ADDRESS } from '@config';
-import provider from '@provider/bscProvider';
-import ethers from 'ethers';
-
-export default new ethers.Contract(CONTRACT_ADDRESS, abi, provider);
