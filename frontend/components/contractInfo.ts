b0fb9665abd0d42fd3ad26d50a8e0742e40096f7
wufengtao1 <48203810+wufengtao1@users.noreply.github.com>
2024-03-16 18:15:19 +0800

    Update contractInfo.ts

--- frontend/components/contractInfo.ts
+++ frontend/components/contractInfo.ts
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
 import config from "../config";
 
-export const planetPerPrice = ethers.utils.parseEther("0.000000001");
+export const planetPerPrice = BigNumber.from("1000000000");
 export const planetContractAddress = config.contractAddress;
 export const planetContractAbi = config.contractABI;
